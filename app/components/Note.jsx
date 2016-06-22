af6cb876a9952520660ea739b28472daa646feb8
Michael Rico <rico.michael@gmail.com>
2016-06-22 11:51:04 -0700

    Begin react

---
+++ app/components/Note.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default() => <div>Take me home, country rose</div>;
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const merge = require('webpack-merge');
+const TARGET = process.env.npm_lifecycle_event;
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  resolve: { // dictates which files to resolve modules?
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      }]
+  }
+};
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devTool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.build,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({ save: true })
+    ],
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
