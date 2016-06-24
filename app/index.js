9360e5ecabe838fb8bcba6896151424d522d1a35
Michael Rico <rico.michael@gmail.com>
2016-06-23 22:09:18 -0700

    Implement finalStore

--- app/index.js
+++ app/index.js
@@ -2,6 +2,11 @@ import './main.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
 
+persist(alt, storage, 'app'); // this will trigger the persistency logic
+// at app initialization
 
 ReactDOM.render(<App />, document.getElementById('app'));
---
+++ app/libs/persist.js
@@ -0,0 +1,21 @@
+import makeFinalStore from 'alt-utils/lib/makeFinalStore';
+
+export default function (alt, storage, storeName) {
+  const finalStore = makeFinalStore(alt);
+  // this line possibly decorates or creates a finalStore instance on the
+  // alt instance -- used on line 16 -- will listen for changes to state
+
+  try {
+    // bootstrap is a method of the alt object
+    // this fetches the stored final store in localStorage
+    alt.bootstrap(store.get(storeName));
+  } catch(e) {
+    console.error('Failed to bootstrap', e);
+  }
+
+  finalStore.listen(() => {
+    if (!storage.get('debug')) {
+      storage.set(storeName, alt.takeSnapShot());
+    }
+  });
+}
