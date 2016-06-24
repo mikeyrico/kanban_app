0df091baf54a29727dd42b6b7791231240ecc6ca
Michael Rico <rico.michael@gmail.com>
2016-06-23 22:15:15 -0700

    fix bug in persist

--- app/libs/persist.js
+++ app/libs/persist.js
@@ -1,6 +1,7 @@
 import makeFinalStore from 'alt-utils/lib/makeFinalStore';
 
 export default function (alt, storage, storeName) {
+  // Final store listens to all existing stores for changes
   const finalStore = makeFinalStore(alt);
   // this line possibly decorates or creates a finalStore instance on the
   // alt instance -- used on line 16 -- will listen for changes to state
@@ -8,14 +9,14 @@ export default function (alt, storage, storeName) {
   try {
     // bootstrap is a method of the alt object
     // this fetches the stored final store in localStorage
-    alt.bootstrap(store.get(storeName));
+    alt.bootstrap(storage.get(storeName));
   } catch(e) {
     console.error('Failed to bootstrap', e);
   }
 
   finalStore.listen(() => {
     if (!storage.get('debug')) {
-      storage.set(storeName, alt.takeSnapShot());
+      storage.set(storeName, alt.takeSnapshot());
     }
   });
 }
